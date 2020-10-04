2ed7bae71a19f265edfad480dcb17e4523c4212b
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 13:21:07 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * a program to determine what percentage grade you got
+ * @author Daniel Rahmani
  */
 public class Main {
 
@@ -9,7 +11,29 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // get the grade from the user
+    System.out.println("Please enter your letter grade. (A, B, C, D, F)");
+    String userGrade = input.nextLine();
+
+    switch(userGrade) {
+      case "A": 
+        System.out.println("Your grade is between 80% and 100%");
+        break;
+      case "B":
+        System.out.println("Your grade is between 70% and 79%");
+        break;
+      case "C":
+        System.out.println("Your grade is between 60% and 69%");
+        break;
+      case "D":
+        System.out.println("Your grade is between 50% and 59%");
+        break;
+      case "F":
+        System.out.println("Your grade is between 0% and 49%");
+    }
     
   }
 }
